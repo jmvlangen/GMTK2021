df4ee11444468f4b94be72752a0a725a4d28dc27
S2108887 <23032053+S2108887@users.noreply.github.com>
2021-06-11 22:19:46 +0200

    Joint probeersel

---
+++ Body.gd
@@ -0,0 +1,19 @@
+extends RigidBody2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+func _integrate_forces(state):
+	if Input.is_action_just_pressed("move_left"):
+		state.apply_impulse(Vector2(0,-50) ,Vector2(-100,0));
